710e680fd7b5e34a72ff848f5b9627962b2c1716
Bello J <bellfulani26@gmail.com>
2016-04-21 15:22:03 +0100

    commit

--- src/app/studentMgmt/StudentAddController.js
+++ src/app/studentMgmt/StudentAddController.js
@@ -1,26 +1,3 @@
 /**
  * Created by Bello J on 4/21/2016.
  */
-(function () {
-    'use strict';
-    angular.module('fuoPortal')
-        .controller('StudentAddController',function(Student,toastr,$modalInstance){
-            var vm = this;
-
-            vm.ok = function(){
-                if(vm.form.$valid){
-                    Student.add(vm.matricNo,vm.firstName,vm.middleName,vm.lastName,vm.collegeId,vm.departmentId,vm.majorId,vm.level,vm.mode_of_entry,vm.session,vm.dateBirth,vm.email,vm.address,vm.password)
-                        .then(function(){
-                            toastr.success("Student Added");
-                            $modalInstance.close();
-                        })
-                        .catch(function(){
-                            toastr.error("Unable to Add Student");
-                        });
-                }
-                else{
-                    toastr.error("Errors in form");
-                }
-            };
-        });
-})();
